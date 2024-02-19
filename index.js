a77c17f620d4e85d46fd33be11cd48f3e61c07a9
bhoopendra3shukla <bhoopendrashukla3@gmail.com>
2024-02-19 11:59:40 +0530

    delete the unuse code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const express=require("express");
+import  express from'express';
 const app=express();
  const port =process.env.PORT ||3000;
 console.log(process.env.PORT,"process.env.PORT")
@@ -9,16 +9,4 @@ console.log(`app is listen the port =${port}`)
 
 app.get('/',(req,res)=>{
     res.send("<h1>This is the testing</h1>")
-});
-
-app.get('/twitter',(req,res)=>{
-    res.send("<h1>This is the twitter</h1>")
-});
-
-app.get('/login',(req,res)=>{
-    res.send("<h1>This is the login code</h1>")
-});
-
-app.get('/signup',(req,res)=>{
-    res.send("<h1>This is signup page </h1>")
 });
